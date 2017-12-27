aef1159257f9942e2fb9cdaf2d3cb7e25d0aee4e
apocalypse575 <apocalypse575@gmail.com>
2017-12-27 20:58:02 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,6 +1,10 @@
 # your code goes here
-def begins_with_r
-
+def begins_with_r(element)
+  if element.startwith?("r")
+    true
+  else
+    false
+  end
 end
 
 #begins_with_r Return true if every element of the tools array starts with an "r" and false otherwise.
